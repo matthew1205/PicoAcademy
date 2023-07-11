f6494dce057f2a924e7e8ad8bd6a0f8c4986c514
matthew1205 <matthew@wiznet.io>
2023-07-12 00:46:32 +0900

    Add files via upload

---
+++ SSD1306_Hello_World/SSD1306_Hello_World.ino
@@ -0,0 +1,50 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int i = 0;
+
+void setup() {
+  
+  Wire.setSDA(12);
+  Wire.setSCL(13);
+  Wire.begin();  
+  
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("!!!Hello, world!");
+  display.display(); 
+  delay(1000);
+}
+
+void loop() {
+
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.println(i);
+  display.display();
+  i++;
+  delay(1000);
+}
